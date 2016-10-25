176bafe5c9fbacdc430b85ac5d0a0f0c53fd0f9e
Nik Strauss <nikcstrauss@gmail.com>
2016-10-25 11:30:43 -0500

    Added testing features to master.

--- calc.py
+++ calc.py
@@ -2,8 +2,8 @@ print ('Welcome to NikCalc!')
 x = input('Enter your first number:' )
 op = input('Enter the operand: ')
 y = input('Enter the second number: ')
-x = int(x)
-y = int(y)
+x = float(x)
+y = float(y)
 if op == ('+'):
 	print (x + y)
 elif op == ('-'):
